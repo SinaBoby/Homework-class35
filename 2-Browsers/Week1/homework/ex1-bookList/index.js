29c33ed647d95e27d77d388c0c80f993f213944a
Sina Boby <sinaboby91@yahoo.com>
2022-01-18 15:26:22 +0100

    unit test ex1 passed

--- 2-Browsers/Week1/homework/ex1-bookList/index.js
+++ 2-Browsers/Week1/homework/ex1-bookList/index.js
@@ -18,7 +18,49 @@ https: //hyf-js2-week1-makeme-ex1-demo.herokuapp.com/
 //cspell: enable
 
 function createBookList(books) {
-  // TODO your code goes in here, return the ul element
+  const bookList = document.createElement('ul');
+  document.getElementById('bookList').appendChild(bookList);
+
+  for (const book of books) {
+    const bookTitle = document.createElement('p');
+    bookTitle.textContent = `${book.title} - ${book.author}`;
+    const item = document.createElement('li');
+    bookList.appendChild(item);
+    item.appendChild(bookTitle);
+    item.style.listStyle = 'none';
+    Object.assign(bookList.style, {
+      listStyle: 'none',
+      display: 'flex',
+      flexWrap: 'wrap',
+      padding: '20px',
+      width: 'calc( 100% - 41px)',
+    });
+    if (book.alreadyRead === true) {
+      item.style.backgroundColor = 'green';
+    } else {
+      item.style.backgroundColor = 'red';
+    }
+    item.style.width = 'calc(25% - 51px)';
+    item.style.padding = '10px';
+    item.style.minWidth = '350px';
+    item.style.margin = '15px';
+    const image = document.createElement('img');
+
+    Object.assign(image.style, {
+      width: '70%',
+      height: '85%',
+    });
+    item.appendChild(image);
+  }
+
+  console.log(document.querySelectorAll('img'));
+  const images = document.querySelectorAll('img');
+  images[0].setAttribute('src', './assets/the_design_of_everyday_things.jpg');
+  images[0].setAttribute('alt', 'Don-Norman-book');
+  images[1].setAttribute('src', './assets/the_most_human_human.jpg');
+  images[1].setAttribute('alt', 'Brian_book');
+  images[2].setAttribute('src', './assets/the_pragmatic_programmer.jpg');
+  images[2].setAttribute('alt', 'Andrew-book');
 }
 
 function main() {
